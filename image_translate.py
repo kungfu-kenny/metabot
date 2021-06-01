c85bf813a55ff51660ff6d0001658eeace079c9d
kungfu-kenny <oleksandrshevchenko889@gmail.com>
2021-06-01 17:56:47 +0300

    first version of the dictionary

--- image_translate.py
+++ image_translate.py
@@ -45,6 +45,26 @@ class ImageMetadata:
         """
         pass
 
+    def merge_metadate(self, value_pil:dict, value_exif:dict) -> dict:
+        """
+        Method which is dedicated to make a merge of two different types of the metadate
+        Input:  value_pil = dictionary which was taken from the PIL library
+                value_exif = dictionary which was taken directly from the EXIF
+        Output: one dictionary with two types of the 
+        """
+        for key_exif in list(value_exif.keys()):
+            if 'EXIF ' in key_exif:
+                val_exif = value_exif.pop(key_exif)
+                value_exif[key_exif.replace('EXIF ', '')] = val_exif
+            if 'Image ' in key_exif:
+                val_exif = value_exif.pop(key_exif)
+                value_exif[key_exif.replace('Image ', '')] = val_exif
+            #TODO think what to do with this type of the data
+            if 'Thumbnail ' in key_exif:
+                value_exif.pop(key_exif)
+        value_pil.update(value_exif)
+        return value_pil
+
     def check_img(self) -> dict:
         """
         Method which looks on the tags and returns them in cases of the 
@@ -54,11 +74,14 @@ class ImageMetadata:
         for value_file in os.listdir(self.folder_img):
             value_file_path = os.path.join(self.folder_img, value_file)
             value_pil = self.get_list_metadate_pil(value_file_path)
-            print(value_pil)
-            print('==========================================================')
+            # print(value_pil)
+            # print('==========================================================')
             value_exif = self.get_list_metadate_exifread(value_file_path)
-            pprint(value_exif)
-            print('##################################################################')
+            # pprint(value_exif)
+            # print('##################################################################')
+            value_meta = self.merge_metadate(value_pil, value_exif)
+            pprint(value_meta)
+            print('#######################################################')
             
 
 if __name__ == '__main__':
